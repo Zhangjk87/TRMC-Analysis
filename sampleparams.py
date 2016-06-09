67e5fbd892285c970d51628e9bd975a555e6a1a5
Kevin Zhang <zhangjk8@yahoo.com>
2016-06-09 10:55:48 -0400

    Commit

---
+++ sampleparams.py
@@ -0,0 +1,19 @@
+
+Fa=0.780141844#fraction of light absorbed
+
+P0offset=0#volts
+er=16.0#relative permittivity
+illuminationFactor=2.00
+lightReachingSample=5.56*0.906*10**-4*0.92307525#last number is transmission through 10 mm BK7 at 530 nm; note that lens is 3.5 mm thick in center, so this is approx. Thorlabs says, however, that virtually all loss is reflection, so likely an accurate approximation
+spotdiameter=9e-3#cm
+wavelength=532e-9#nm
+
+exponential=1
+a=1
+t1=200e-9
+offset=0
+
+#for charge per QD
+radius=4.6e-9/2
+packingFraction=.5
+thickness=214.59e-9#meters
